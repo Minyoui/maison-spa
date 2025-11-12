902fdfb2b342faad0f72f2e0cb3ecc59e9ddcbb5
Minyoui <evanlyrush73@gmail.com>
2025-11-13 01:40:54 +0800

    9th Commit (About Section Setup)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-
 // Sections
 import Hero from "@/sections/hero-section";
+import About from "@/sections/about-section";
 
 export default function Home() {
   return (
     <main>
         <Hero />
+        <About />
     </main>
   );
 }
